451f579ecbfc8ebaea944db5cb71f1bbb47b27fe
Maciej Fiałkowski <Fialek85@gmail.com>
2019-03-11 19:34:19 +0100

    added react-router

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch,
+    NavLink,
+} from 'react-router-dom';
 
 
 
@@ -10,15 +17,17 @@ const Login = () => {
         <>
             <div className='login'>
                 <div className='login__left' >
-                    <div className='login__content'>
-                        <FontAwesomeIcon icon={faUserCircle} className="loginIcon" />
-                        <div className='login__text dark'>Zaloguj się</div>
-                    </div>
+                    <Link to='/contact'>
+                        <div className='login__content' >
+                            <FontAwesomeIcon icon={faUserCircle} className="loginIcon" />
+                            <div className='login__text dark'>Dodaj klienta</div>
+                        </div>
+                    </Link>
                 </div>
                 <div className='login__right'>
                     <div className='login__content'>
                         <FontAwesomeIcon icon={faUserCircle} className="loginIcon white" />
-                        <div className='login__text white'>Zarejestruj się</div>
+                        <div className='login__text white'>Poprzednie sesje</div>
                     </div>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './components/login';
+import Contact from './components/contact';
+import NotFound from './components/notFound';
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch,
+    NavLink,
+} from 'react-router-dom';
 import './scss/main.scss';
+
 
 
 class App extends React.Component {
@@ -13,11 +23,20 @@ class App extends React.Component {
 
     render() {
         return (
-            <Login />
-        )
+            <HashRouter>
+                <Switch>
+                    <Route exact path='/' component={Login} />
+                    <Route path='/' component={Contact} />
+                    <Route component={NotFound} />
+                </Switch>
+            </HashRouter>
+        );
 
     }
 }
+
+
+
 
 
 ReactDOM.render(
